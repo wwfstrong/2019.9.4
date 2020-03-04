786be2abb8ababc47e356277e0fd5edadd8f2056
wwf <865475679@qq.com>
2020-03-04 16:25:21 +0800

    修改

--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 var body = document.body;
-var deng = document.getElementById('deng');
+var deng = document.getElementById("deng");
 var light = true;
+var els = document.querySelectorAll("li");
 
-var switchLight = function(){
-  if (light){
-    body.className = 'night';
+var switchLight = function() {
+  if (light) {
+    body.className = "night";
     light = false;
-  }
-  else {
-    body.removeAttribute('class');
+    setRandomColor();
+  } else {
+    body.removeAttribute("class");
     light = true;
+    clearRandomColor();
   }
-}
+};
 
 deng.onclick = switchLight;
+
+function setRandomColor() {
+  els.forEach((el, index) => {
+    var r = (Math.random() * 256) | 0;
+    var g = (Math.random() * 256) | 0;
+    var b = (Math.random() * 256) | 0;
+    el.style.color = `rgba(${r},${g},${b})`;
+  });
+}
+
+function clearRandomColor() {
+  els.forEach((el, index) => {
+    el.style.color = null;
+  });
+}
 
 
